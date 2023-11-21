a2a737b70ff65f3ace56e199817ed6a52718013c
tirthbha <143649367+tirthbha@users.noreply.github.com>
2023-11-21 14:03:17 -0500

    Update unit_test_task1.py

--- unit_test_task1.py
+++ unit_test_task1.py
@@ -1,69 +1,36 @@
 import pytest
 import numpy as np
-from calculus_trithbha_version import *
-
+from calculus_trithbha_version import exp, cos, cubic, simpson, trapezoid, adaptive_trapezoid  
 # Test for the exp function
 def test_exp():
-    assert exp(0) == 0  # Check if exp function returns 0 for input 0
-    assert exp(1) == np.exp(-1)  # Check if exp function returns e^-1 for input 1
-    assert np.isclose(exp(np.inf), 0)  # Check if exp function returns close to 0 for input infinity
+    assert exp(0) == 0
+    assert exp(1) == np.exp(-1)
+    assert np.isclose(exp(np.inf), 0)
 
 # Test for the cos function
 def test_cos():
-    assert cos(0) == 1  # Check if cos function returns 1 for input 0
-    assert np.isclose(cos(1), np.cos(1))  # Check if cos function returns cos(1) for input 1
-    assert np.isclose(cos(np.inf), np.cos(0))  # Check if cos function returns cos(0) for input infinity
+    assert cos(0) == 1
+    assert np.isclose(cos(1), np.cos(1))
+    assert np.isclose(cos(np.inf), np.cos(0))
 
 # Test for the cubic function
 def test_cubic():
-    assert cubic(1, 0.5) == 1.5  # Check if cubic function returns correct value for input 1 with constant 0.5
-    assert cubic(0) == 0.5  # Check if cubic function returns constant value (0.5 by default) for input 0
-    assert cubic(-1, -0.5) == -0.5  # Check if cubic function returns correct value for input -1 with constant -0.5
+    assert cubic(1, 0.5) == 1.5
+    assert cubic(0) == 0.5
+    assert cubic(-1, -0.5) == -0.5
 
 # Test for the simpson function
 def test_simpson():
-    result, _ = simpson(np.sin, 0, np.pi, 100, 1e-6)  # Apply simpson rule to integrate sin from 0 to pi
-    assert np.isclose(result, 2, atol=1e-6)  # Check if result is close to 2 with a tolerance of 1e-6
+    result, _ = simpson(np.sin, 0, np.pi, 100, 1e-6)
+    assert np.isclose(result, 2, atol=1e-6)
 
 # Test for the trapezoid function
 def test_trapezoid():
-    result, _ = trapezoid(np.sin, 0, np.pi, 100, 1e-6)  # Apply trapezoid rule to integrate sin from 0 to pi
-    assert np.isclose(result, 2, atol=1e-6)  # Check if result is close to 2 with a tolerance of 1e-6
+    result, _ = trapezoid(np.sin, 0, np.pi, 100, 1e-6)
+    assert np.isclose(result, 2, atol=1e-6)
 
 # Test for the adaptive_trapezoid function
 def test_adaptive_trapezoid():
-    result, _ = adaptive_trapezoid(np.sin, 0, np.pi, 1e-6)  # Apply adaptive trapezoid rule to integrate sin from 0 to pi
-    assert np.isclose(result, 2, atol=1e-6)  # Check if result is close to 2 with a tolerance of 1e-6
-
-
-def test_exp_edge_cases():
-    assert exp(-1) == np.exp(-1)  # Check exp for negative input
-    assert exp(np.nan) != exp(np.nan)  # Check exp for NaN input (result should also be NaN)
-
-# Additional tests for cos function
-def test_cos_edge_cases():
-    assert cos(-1) == np.cos(-1)  # Check cos for negative input
-    assert cos(np.nan) != cos(np.nan)  # Check cos for NaN input (result should also be NaN)
-
-# Additional tests for cubic function
-def test_cubic_edge_cases():
-    assert cubic(2) == 8 + 0.5  # Check cubic for positive input
-    assert cubic(-2) == -8 + 0.5  # Check cubic for negative input
-    assert cubic(np.nan) == np.nan  # Check cubic for NaN input
-
-# Additional tests for simpson function
-def test_simpson_edge_cases():
-    result, _ = simpson(lambda x: x**2, -1, 1, 100, 1e-6)  # Integrate x^2 from -1 to 1
-    assert np.isclose(result, 2/3, atol=1e-6)  # Check if result is close to 2/3
-
-# Additional tests for trapezoid function
-def test_trapezoid_edge_cases():
-    result, _ = trapezoid(lambda x: x**2, -1, 1, 100, 1e-6)  # Integrate x^2 from -1 to 1
-    assert np.isclose(result, 2/3, atol=1e-6)  # Check if result is close to 2/3
-
-# Additional tests for adaptive_trapezoid function
-def test_adaptive_trapezoid_edge_cases():
-    result, _ = adaptive_trapezoid(lambda x: x**2, -1, 1, 1e-6)  # Integrate x^2 from -1 to 1
-    assert np.isclose(result, 2/3, atol=1e-6)  # Check if result is close to 2/3
-
+    result, _ = adaptive_trapezoid(np.sin, 0, np.pi, 1e-6)
+    assert np.isclose(result, 2, atol=1e-6)
 
