c8dbae8a3d361bb626d39a97bbe1ee3c5a29da4c
tirthbha <143649367+tirthbha@users.noreply.github.com>
2023-11-21 14:51:35 -0500

    Update unit_test_task1.py

--- unit_test_task1.py
+++ unit_test_task1.py
@@ -51,3 +51,33 @@ def test_adaptive_trapezoid():
     result, _ = adaptive_trapezoid(np.sin, 0, np.pi, 1e-6)
     assert np.isclose(result, 2, atol=1e-6)
 
+# Test for the 'exp' function with a different function as input
+def test_exp_with_function():
+    # Define a test function (lambda function) that squares its input
+    test_function = lambda x: x**2
+
+    # Test the 'exp' function by applying the test_function to 0.
+    # Since test_function(0) is 0, exp(-1/0) should ideally be 0.
+    # However, due to handling in 'exp', it should return np.exp(-1/small_number) for small inputs.
+    assert np.isclose(exp(test_function(0)), np.exp(-1/test_function(1e-15)))
+
+    # Test the 'exp' function with the test_function applied to 1.
+    # Here, test_function(1) is 1, so exp(-1/1) should return np.exp(-1), 
+    # which is the expected behavior for input 1 to the 'exp' function.
+    assert np.isclose(exp(test_function(1)), np.exp(-1))
+
+# Test for the 'cubic' function with a different function as input
+def test_cubic_with_function():
+    # Define a linear test function (lambda function) that represents a simple linear relationship
+    test_function = lambda x: 2*x + 3
+
+    # Test the 'cubic' function with the test_function applied to 0.
+    # The expected result is the cube of the test function's output at 0 plus a constant (0.5).
+    # Since test_function(0) is 3, cubic(3) should equal 3^3 + 0.5.
+    assert cubic(test_function(0)) == test_function(0)**3 + 0.5
+
+    # Test the 'cubic' function with the test_function applied to 1.
+    # The expected result is the cube of the test function's output at 1 plus a constant (0.5).
+    # Since test_function(1) is 5, cubic(5) should equal 5^3 + 0.5.
+    assert cubic(test_function(1)) == test_function(1)**3 + 0.5
+
